2de847086eb527680240f869e61ec7fa1c2b3144
flacrabe <f.lacrabere@gmail.com>
2020-01-28 14:59:18 +0100

    test des modules pour le site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OnePageComponent } from './one-page/one-page.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ArticlesComponent } from './articles/articles.component';
+import { FormationComponent } from './formation/formation.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: OnePageComponent
+  },
+  {
+    path: 'accueil',
+    component: OnePageComponent
+  },
+  {
+    path: 'articles',
+    component: ArticlesComponent
+  },
+  {
+    path: 'formation',
+    component: FormationComponent
+  },
+  { path: '**', redirectTo: '' },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    OnePageComponent,
+    ArticlesComponent,
+    FormationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
